b8ed96faa2e0480544a312ea9bda340fd4f13b68
iante <31571006+iante@users.noreply.github.com>
2020-09-16 17:17:31 +0300

    Update DataLayer.js

--- music/src/DataLayer.js
+++ music/src/DataLayer.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useReducer } from "react";
 
 export const DataLayerContext = createContext(); //preparing the data layer
 
-//passing initialState and reducer since they are the parameters datalayer takes in our index.js
+//passing initialState and reducer as datalayer params 
 export const DataLayer = ({initialState, reducer, children
 }) =>(
     <DataLayerContext.Provider  value={useReducer(reducer, initialState)}>
